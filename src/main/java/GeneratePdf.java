a4625de98e6ec267918a4b957ed495efd6362a92
Rashid Jilani <rjilani@skytouchtechnology.com>
2020-01-26 09:07:39 -0600

    refactor the itext code

--- src/main/java/GeneratePdf.java
+++ src/main/java/GeneratePdf.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.StringTokenizer;
 
 import com.itextpdf.layout.property.UnitValue;
@@ -40,27 +41,21 @@ public class GeneratePdf {
         PdfFont bold = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
 
         //Add paragraph to the document
-        document.add(new Paragraph("Hello World!"));
-        document.add(new Paragraph("The quick brown fox jumps over the lazy dog!")).setFont(font);
+        addParagraph(document, font);
 
-        List list = new List()
-                .setSymbolIndent(12)
-                .setListSymbol("\u2022")
-                .setFont(font);
-// Add ListItem objects
-        list.add(new ListItem("Never gonna give you up"))
-                .add(new ListItem("Never gonna let you down"))
-                .add(new ListItem("Never gonna run around and desert you"))
-                .add(new ListItem("Never gonna make you cry"))
-                .add(new ListItem("Never gonna say goodbye"))
-                .add(new ListItem("Never gonna tell a lie and hurt you"));
-// Add the list
-        document.add(list);
+        // Add ListItem
+        AddList(document, font);
 
-        Image fox = new Image(ImageDataFactory.create("images/download.png"));
-        Image dog = new Image(ImageDataFactory.create("images/download.png"));
-        document.add(fox);
+        // Add image
+        addImage(document);
 
+        addTable(document, font, bold);
+
+        //Close document
+        document.close();
+    }
+
+    private void addTable(Document document, PdfFont font, PdfFont bold) throws IOException {
         Table table = new Table(new float[]{4, 1, 3, 4, 3, 3, 3, 3, 1});
         table.setWidth(UnitValue.createPercentValue(100));
         BufferedReader br = new BufferedReader(new FileReader("data/states.csv"));
@@ -72,9 +67,34 @@ public class GeneratePdf {
         br.close();
 
         document.add(table);
+    }
 
-        //Close document
-        document.close();
+    private void addImage(Document document) throws MalformedURLException {
+        Image image1 = new Image(ImageDataFactory.create("images/googleimage.png"));
+        Image image2 = new Image(ImageDataFactory.create("images/googleimage2.png"));
+        document.add(image1);
+        document.add(image2);
+    }
+
+    private void AddList(Document document, PdfFont font) {
+        List list = new List()
+                .setSymbolIndent(12)
+                .setListSymbol("\u2022")
+                .setFont(font);
+
+        list.add(new ListItem("Never gonna give you up"))
+                .add(new ListItem("Never gonna let you down"))
+                .add(new ListItem("Never gonna run around and desert you"))
+                .add(new ListItem("Never gonna make you cry"))
+                .add(new ListItem("Never gonna say goodbye"))
+                .add(new ListItem("Never gonna tell a lie and hurt you"));
+
+        document.add(list);
+    }
+
+    private void addParagraph(Document document, PdfFont font) {
+        document.add(new Paragraph("Hello World!"));
+        document.add(new Paragraph("The quick brown fox jumps over the lazy dog!")).setFont(font);
     }
 
     public void process(Table table, String line, PdfFont font, boolean isHeader) {
